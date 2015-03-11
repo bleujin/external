153366f817ccb6e69b7f28139034632b217ca056
bleujin@gmail.com <bleujin@gmail.com>
2015-03-11 15:18:59 +0900

    ..

--- test/net/ion/external/TestExternalServer.java
+++ test/net/ion/external/TestExternalServer.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import junit.framework.TestCase;
 import net.ion.external.ExternalServer;
+import net.ion.external.config.builder.ConfigBuilder;
 import net.ion.external.domain.DomainMaster;
 import net.ion.framework.db.manager.OracleDBManager;
 import net.ion.framework.util.InfinityThread;
@@ -12,7 +13,7 @@ public class TestExternalServer extends TestCase {
 
 
     public void testRun() throws Exception {
-		final ExternalServer server = ExternalServer.create(9001) ;
+		final ExternalServer server = ExternalServer.create(ConfigBuilder.createDefault(9001).build()) ;
 
 		// simmul dmaster
 		DomainMaster dmaster = DomainMaster.create(new OracleDBManager("jdbc:oracle:thin:@dev-oracle.i-on.net:1521:dev10g", "dev_ics6", "dev_ics6"), server.craken())
