985e5585132a73f99f82722a56ccac5e1e7c8e3c
JaeHyuk, Kim <airkjh99@gmail.com>
2015-04-22 10:28:49 +0900

    resize function added

--- webapps/admin/js/plugin/superbox/superbox.min.custom.js
+++ webapps/admin/js/plugin/superbox/superbox.min.custom.js
@@ -2,7 +2,7 @@
 !function (a) {
     a.fn.SuperBox = function () {
         var b = a('<div class="superbox-show"></div>'),
-            c = a('<img src="" class="superbox-current-img"><div id="imgInfoBox" class="superbox-imageinfo inline-block"> <h1>Image Title</h1><span><p><em>http://imagelink.com/thisimage.jpg</em></p><p class="superbox-img-description">Image description</p><p><a href="javascript:void(0);" class="btn btn-primary btn-sm edit-image">Crop Image</a></span> </div>'),
+            c = a('<img src="" class="superbox-current-img"><div id="imgInfoBox" class="superbox-imageinfo inline-block"> <h1>Image Title</h1><span><p><em>http://imagelink.com/thisimage.jpg</em></p><p class="superbox-img-description">Image description</p><p><a href="javascript:void(0);" class="btn btn-primary btn-sm crop-image">Crop Image</a></span>&nbsp;<a href="javascript:void(0);" class="btn btn-info btn-sm resize-image">Resize Image</a></span> </div>'),
             d = a('<div class="superbox-close txt-color-white"><i class="fa fa-times fa-lg"></i></div>');
         b.append(c).append(d);
         a(".superbox-imageinfo");
@@ -26,7 +26,8 @@
 
                 // crop modal by airkjh. cropHandler defined in gallery.html
                 c.attr("src", e), a(".superbox-list").removeClass("active"), $this.addClass("active"), c.find("em").text(g), c.find(">:first-child").text(h), c.find(".superbox-img-description").text(f), 0 == a(".superbox-current-img").css("opacity") && a(".superbox-current-img").animate({opacity: 1}), a(this).next().hasClass("superbox-show") ? (a(".superbox-list").removeClass("active"), b.toggle()) : (b.insertAfter(this).css("display", "block"), $this.addClass("active")), a("html, body").animate({scrollTop: b.position().top - d.width()}, "medium");
-                c.find('a.edit-image').click({basic: basicData, src: e, originalWidth: c.naturalWidth, originalHeight: c.naturalHeight}, cropHandler);
+                c.find('a.crop-image').click({basic: basicData, src: e, originalWidth: c.naturalWidth, originalHeight: c.naturalHeight}, cropHandler);
+                c.find('a.resize-image').click({basic: basicData, src: e, originalWidth: c.naturalWidth, originalHeight: c.naturalHeight}, resizeHandler) ;
 
             }), a(".superbox").on("click", ".superbox-close", function () {
                 a(".superbox-list").removeClass("active"), a(".superbox-current-img").animate({opacity: 0}, 200, function () {
